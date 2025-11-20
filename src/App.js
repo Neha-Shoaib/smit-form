b3369715359c27702a6f70521282b3fa970baf23
Neha Shoaib <thisisnehashoaib@gmail.com>
2025-11-20 23:26:38 +0500

    fix build and dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from './components/Form'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div>
